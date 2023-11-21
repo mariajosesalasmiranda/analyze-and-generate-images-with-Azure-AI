e64fe1de3d33fc6df55282dc8f65632d5395e265
Maria José Salas Miranda <mariajose.salasmiranda@gmail.com>
2023-11-22 00:35:34 +0100

    last time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// title
 import React, { useState } from 'react';
+import analyzeImage from './azure-image-analysis';
 
+// Simple GUI
 function App() {
   const [imageUrl, setImageUrl] = useState('');
   const [imagePrompt, setImagePrompt] = useState('');
@@ -32,5 +33,43 @@ function App() {
   );
 }
 
-export default App;
+// Analyze an image
+const App = () => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [processing, setProcessing] = useState(false);
+  const [analysisResults, setAnalysisResults] = useState(null);
 
+  const handleAnalyzeImage = async () => {
+    setProcessing(true);
+    const results = await analyzeImage(imageUrl);
+    setProcessing(false);
+    setAnalysisResults(results);
+  };
+
+  const displayResults = () => {
+    if (!analysisResults) {
+      return <div>No analysis results yet.</div>;
+    }
+
+    const { categories, description, tags } = analysisResults;
+    return (
+      <div>
+        <p>Image URL: {imageUrl}</p>
+        <p>Categories: {categories.join(', ')}</p>
+        <p>Description: {description}</p>
+        <p>Tags: {tags.join(', ')}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <input type="text" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+      <button onClick={handleAnalyzeImage} disabled={processing}>Analyze</button>
+      {processing && <div>Analyzing image...</div>}
+      {analysisResults && displayResults()}
+    </div>
+  );
+};
+
+export default App;
