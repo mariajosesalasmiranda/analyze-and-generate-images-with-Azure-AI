fd7f23f416eebe24a087f2f0a28e3207bdf6c7a4
Maria José Salas Miranda <mariajose.salasmiranda@gmail.com>
2023-11-21 22:16:03 +0100

    try again

--- src/App.js
+++ src/App.js
@@ -2,43 +2,38 @@ import React, { useState } from 'react';
 
 function App() {
   const [imageUrl, setImageUrl] = useState('');
-  const [prompt, setPrompt] = useState('');
+  const [promptText, setPromptText] = useState('');
 
   const handleImageUrlChange = (event) => {
     setImageUrl(event.target.value);
   };
 
-  const handlePromptChange = (event) => {
-    setPrompt(event.target.value);
+  const handlePromptTextChange = (event) => {
+    setPromptText(event.target.value);
   };
 
   const handleImageAnalysis = () => {
-    // Logic to trigger image analysis
-    // You can use 'imageUrl' to access the entered URL
-    console.log('Image analysis triggered with URL:', imageUrl);
+    // Add code to trigger image analysis (will be implemented later)
+    console.log('Image analysis triggered');
   };
 
   const handleImageGeneration = () => {
-    // Logic to trigger image generation
-    // You can use 'prompt' to access the entered prompt
-    console.log('Image generation triggered with prompt:', prompt);
+    // Add code to trigger image generation (will be implemented later)
+    console.log('Image generation triggered');
   };
 
   return (
     <div className="App">
       <h1>Title</h1>
-      <label>
-        Enter Image URL:
-        <input type="text" value={imageUrl} onChange={handleImageUrlChange} />
-      </label>
+      <input
+        type="text"
+        placeholder="Enter image URL or prompt text"
+        value={imageUrl}
+        onChange={handleImageUrlChange}
+      />
       <br />
-      <label>
-        Enter Image Prompt:
-        <input type="text" value={prompt} onChange={handlePromptChange} />
-      </label>
-      <br />
-      <button onClick={handleImageAnalysis}>Trigger Image Analysis</button>
-      <button onClick={handleImageGeneration}>Trigger Image Generation</button>
+      <button onClick={handleImageAnalysis}>Image Analysis</button>
+      <button onClick={handleImageGeneration}>Image Generation</button>
     </div>
   );
 }
